08cce57966fe9847b3d3fd04f4cc7ddf336dc976
zDR9YHt5ooXU <git1@kaiguo.ca>
2021-12-31 11:24:19 -0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+function selectionUpdated(params: { a?: string; b?: string }) {
+  const c = params.b && params.a;
+  if (c) {
+    [].forEach(() => {
+      if (params.a) {
+        unsetA(params);
+        getString(params.a);
+      }
+      if (c) {
+        getString(params.a);
+      }
+      getString(params.b);
+    });
+  }
+}
+
+function unsetA(params: { a?: string; b?: string }) {
+  delete params.a;
+}
+
+function getString(v: string) {
+  console.log(v);
+}
